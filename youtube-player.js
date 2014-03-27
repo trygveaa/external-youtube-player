9052be08439ae110c94b61f77cf45980287ad421
Trygve Aaberge <trygveaa@gmail.com>
2014-03-27 13:55:30 +0100

    Stop autoplaying videos and mute ads

--- youtube-player.js
+++ youtube-player.js
@@ -1,25 +1,68 @@
+var player;
+var video;
+var firstPause = true;
+
+function adShowing() {
+    if (player) {
+        return player.classList.contains("ad-showing");
+    }
+    return false;
+}
+
+function handlePlay() {
+    if (adShowing()) {
+        firstPause = true;
+        video.muted = true;
+    } else if (firstPause) {
+        firstPause = false;
+        video.pause();
+        video.muted = false;
+    }
+}
+
+function stopVideo() {
+    player = document.getElementById("movie_player");
+    if (player) {
+        video = player.getElementsByTagName("video")[0];
+        if (video) {
+            video.addEventListener("play", handlePlay);
+            video.addEventListener("playing", handlePlay);
+            video.addEventListener("emptied", function () { firstPause = true; });
+        }
+    }
+}
+
 function insertPlayerLink() {
-    var link = document.URL.replace(/https?/, "youtube");
-
-    var playerButton = document.createElement("span");
-    playerButton.setAttribute("class", "yt-uix-button yt-uix-button-text");
-    playerButton.setAttribute("style", "font-size: 1.5em; margin-left: 2.5em;");
-    playerButton.innerHTML = "Play externally";
-
-    var playerLink = document.createElement("a");
-    playerLink.setAttribute("href", link);
-    playerLink.setAttribute("style", "text-decoration: none;");
-    playerLink.appendChild(playerButton);
-
     var headline = document.getElementById("watch7-headline");
-    headline.appendChild(playerLink);
+    if (headline) {
+        var link = document.URL.replace(/https?/, "youtube");
+
+        var playerButton = document.createElement("span");
+        playerButton.setAttribute("class", "yt-uix-button yt-uix-button-text");
+        playerButton.setAttribute("style", "font-size: 1.5em; margin-left: 2.5em;");
+        playerButton.innerHTML = "Play externally";
+
+        var playerLink = document.createElement("a");
+        playerLink.setAttribute("href", link);
+        playerLink.setAttribute("style", "text-decoration: none;");
+        playerLink.appendChild(playerButton);
+
+        headline.appendChild(playerLink);
+    }
+}
+
+function documentLoaded() {
+    stopVideo();
+    insertPlayerLink();
 }
 
 function handleInsertEvent(e) {
-    if (e.target.nodeName === "DIV" && e.target.id === "watch7-container") {
+    if (e.target.nodeName === "VIDEO") {
+        stopVideo();
+    } else if (e.target.nodeName === "DIV" && e.target.id === "watch7-container") {
         insertPlayerLink();
     }
 }
 
-document.addEventListener("DOMContentLoaded", insertPlayerLink, true);
+document.addEventListener("DOMContentLoaded", documentLoaded, true);
 document.addEventListener("DOMNodeInserted", handleInsertEvent, true);
